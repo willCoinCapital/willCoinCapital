16ce1abac3ec4ddb11e54e9e17d2822c9d8ec003
willCoinCapital <toyorlagos@gmail.com>
2026-03-13 18:31:38 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,20 @@
+function login(){
 
+let username = document.getElementById("username").value
+let password = document.getElementById("password").value
+
+/* Demo login credentials */
+
+if(username === "admin" && password === "1234"){
+
+localStorage.setItem("willcoinLoggedIn","true")
+
+window.location.href = "dashboard.html"
+
+}else{
+
+alert("Invalid login details")
+
+}
+
+}
